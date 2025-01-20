faf1c9563f4df35cb1835aa059cea6d1dce793f3
snappas <snappas@gmail.com>
2025-01-19 21:42:45 -0600

    Render untextured UI

--- src/renderer_vk/shaders/compile_shaders.bat
+++ src/renderer_vk/shaders/compile_shaders.bat
@@ -1,2 +1,5 @@
-C:\dxc_2024_07_31\bin\x64\dxc.exe -spirv -T vs_6_0 -E vs %cd%\triangle.hlsl -Fh %cd%\triangle_vs.h -Vn triangle_vs
-C:\dxc_2024_07_31\bin\x64\dxc.exe -spirv -T ps_6_0 -E ps %cd%\triangle.hlsl -Fh %cd%\triangle_ps.h -Vn triangle_ps
+set DXC=%VULKAN_SDK%\bin\dxc.exe
+del triangle_vs.h
+del triangle_ps.h
+%DXC% -spirv -T vs_6_0 -E vs %cd%\triangle.hlsl -Fh %cd%\triangle_vs.h -Vn triangle_vs
+%DXC% -spirv -T ps_6_0 -E ps %cd%\triangle.hlsl -Fh %cd%\triangle_ps.h -Vn triangle_ps
