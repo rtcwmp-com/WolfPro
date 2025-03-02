8be565e834833432ac524a1a7f2fa325334d93d0
snappas <snappas@gmail.com>
2025-03-02 16:05:37 -0600

    upload font

--- src/client/cl_imgui.c
+++ src/client/cl_imgui.c
@@ -1,6 +1,7 @@
 #include "cl_imgui.h"
 #include "font_bahnschrift.h"
-#include "client.h"
+#include "client.h" 
+#include <float.h>
 
 static const ImWchar codepointRanges[] =
 {
@@ -15,14 +16,19 @@ static void AddFont()
     int height = 13;
     ImFontConfig config = {};
     config.FontDataOwnedByAtlas = false;
-    config.OversampleH = 1;
-    config.OversampleV = 1;
+    config.OversampleH = 0;
+    config.OversampleV = 0;
     config.PixelSnapH = true;
     config.SizePixels = height;
+
+    config.GlyphMaxAdvanceX = FLT_MAX;
     config.RasterizerDensity = 1.0f;
+    config.RasterizerMultiply = 1.0f;
+
 
     Com_sprintf(config.Name, sizeof(config.Name), "%s (%dpx)", "Banschrift", height);
     ImFontAtlas_AddFontFromMemoryTTF(io->Fonts, bahnschrift_ttf, sizeof(bahnschrift_ttf), config.SizePixels, &config, codepointRanges);
+    //ImFontAtlas_AddFontFromMemoryTTF(io->Fonts, bahnschrift_ttf, sizeof(bahnschrift_ttf), height, NULL, codepointRanges);
 }
 
 void CL_ImGUI_Init(void){
