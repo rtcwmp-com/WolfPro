0e0a9b52c9d37e106cac76accfe0aafaefc660bb
snappas <snappas@users.noreply.github.com>
2025-07-05 13:24:23 -0500

    ndp timeline

--- src/cgame/cg_imgui.c
+++ src/cgame/cg_imgui.c
@@ -8,6 +8,16 @@ void CG_ImGUI_Share(void *ctx, void *alloc, void *free, void **user){
 	cgs.igUserData = user;
 }
 
+char* GetStringTimestamp(int seconds){
+	int hour = seconds / 3600;
+	int minute = (seconds / 60) % 60;
+	int second = seconds % 60;
+	if(hour > 0){
+		return va("%02d:%02d:%02d", hour, minute, second);
+	}
+	return va("%02d:%02d", minute, second);
+}
+
 void CG_ImGUI_Update(void) {
 	if (cgs.igContext == NULL) {
 		return;
@@ -39,19 +49,45 @@ void CG_ImGUI_Update(void) {
 	if(demoTimelineActive){
 		
 		if(igBegin("Timeline", &demoTimelineActive, 0)){
+			
 			int demoDuration = (m_lastServerTime - m_firstServerTime);
 			int currentOffset = (m_currServerTime - m_firstServerTime);
-
 			float percent = ((float)currentOffset / (float)demoDuration) * 100.0f;
 			float origPercent = percent;
 			igPushItemWidth(igGetWindowWidth() - 16.0f);
 			igSliderFloat("##timeline", &percent, 0.0f, 100.0f, "", ImGuiSliderFlags_None);
 			igPopItemWidth();
+			
+			igText("%s / %s", GetStringTimestamp(currentOffset / 1000), GetStringTimestamp(demoDuration / 1000));
+
 			if(percent != origPercent){
 				int serverTimeAtPercent = (int)((float)((m_lastServerTime - m_firstServerTime) * percent / 100.0f)) + m_firstServerTime;
 				CG_NDP_SeekAbsolute(serverTimeAtPercent);
 			}
 			
+			
+			igSameLine(0.0f, 16.0f);
+			if(igButton("Next Frag", (ImVec2){0.0f, 0.0f})){
+				CG_NDP_GoToNextFrag(qtrue);
+			}
+			igSameLine(0.0f, 16.0f);
+
+			if(igButton("Prev Frag", (ImVec2){0.0f, 0.0f})){
+				CG_NDP_GoToNextFrag(qfalse);
+			}
+			igSameLine(0.0f, 16.0f);
+			static qbool paused = qfalse;
+			if(!paused){
+				if(igButton("Pause", (ImVec2){0.0f, 0.0f})){
+					trap_Cvar_Set("timescale", "0");
+					paused = qtrue;
+				}
+			}else{
+				if(igButton("Play", (ImVec2){0.0f, 0.0f})){
+					trap_Cvar_Set("timescale", "1");
+					paused = qfalse;
+				}
+			}
         }
         igEnd();
     }
