99f16eb092d1fce78024e5c7e236ac47d13a2e53
snappas <snappas@users.noreply.github.com>
2025-09-03 17:32:00 -0500

    when revived look the same direction as died

--- src/game/g_client.c
+++ src/game/g_client.c
@@ -1778,7 +1778,7 @@ void ClientSpawn( gentity_t *ent, qboolean revived ) {
 
 	if ( revived ) {
 		spawnPoint = ent;
-		VectorCopy( ent->s.origin, spawn_origin );
+		VectorCopy(ent->r.currentOrigin, spawn_origin); // Issue 24 fix document/revive bug
 		spawn_origin[2] += 9;   // spawns seem to be sunk into ground?
 		VectorCopy( ent->s.angles, spawn_angles );
 	} else
@@ -1980,7 +1980,17 @@ void ClientSpawn( gentity_t *ent, qboolean revived ) {
 	// the respawned flag will be cleared after the attack and jump keys come up
 	client->ps.pm_flags |= PMF_RESPAWNED;
 
-	SetClientViewAngle( ent, spawn_angles );
+	if(revived){
+		vec3_t newangle;
+		//restore the value for the client's view before death
+		newangle[YAW] = client->pers.deathYaw; 
+		newangle[PITCH] = 0;
+		newangle[ROLL] = 0;
+		SetClientViewAngle( ent, newangle );
+	}else{
+		SetClientViewAngle( ent, spawn_angles );
+	}
+	
 
 	if ( ent->client->sess.sessionTeam != TEAM_SPECTATOR ) {
 		//G_KillBox( ent );
