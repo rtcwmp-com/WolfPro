ae244a57b4f3edb97ae72865094f476d7660f11e
snappas <snappas@users.noreply.github.com>
2025-10-18 18:48:09 -0500

    cgame: sync respawn time on various areas (#100)

--- src/cgame/cg_scoreboard.c
+++ src/cgame/cg_scoreboard.c
@@ -319,7 +319,7 @@ int WM_DrawObjectives( int x, int y, int width, float fade ) {
 			tens = seconds / 10;
 			seconds -= tens * 10;
 
-			s = va( "%s %2.0f:%i%i", CG_TranslateString( "Reinforce time:" ), (float)mins, tens, seconds );
+			s = va( "%s 0:%d", CG_TranslateString( "Reinforce time:" ), CG_CalculateReinfTime() );
 			CG_DrawSmallString( x + 425,y,s,fade );
 		}
 
